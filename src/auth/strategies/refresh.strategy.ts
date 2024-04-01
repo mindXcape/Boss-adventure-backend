6a2f5f48956def3b0d9079e0f5ae245c3cfa7714
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2024-04-01 22:05:13 +0545

    fix: fix refresh token bug

--- src/auth/strategies/refresh.strategy.ts
+++ src/auth/strategies/refresh.strategy.ts
@@ -4,7 +4,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 export class RefreshJWTStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromBodyField('refresh'),
+      jwtFromRequest: ExtractJwt.fromBodyField('refresh_token'),
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_SECRET_KEY,
     });
